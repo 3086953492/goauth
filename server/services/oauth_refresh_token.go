b2a0ff936882dbe3c705ea2e5bf1993e56948836
3086953492 <3086953492@qq.com>
2025-12-04 13:48:16 +0800

    fix: 修正刷新令牌生成失败的错误信息

--- server/services/oauth_refresh_token.go
+++ server/services/oauth_refresh_token.go
@@ -24,7 +24,7 @@ func NewOAuthRefreshTokenService(oauthRefreshTokenRepository *repositories.OAuth
 func (s *OAuthRefreshTokenService) GenerateRefreshToken(ctx context.Context, accessTokenID uint, clientID string, scope string, userID uint, username string, role string) (string, error) {
 	refreshTokenString, err := jwt.GenerateToken(strconv.FormatUint(uint64(userID), 10), username, map[string]any{"role": role})
 	if err != nil {
-		return "", errors.Internal().Msg("生成访问令牌失败").Err(err).Log()
+		return "", errors.Internal().Msg("生成刷新令牌失败").Err(err).Log()
 	}
 
 	refreshToken := &models.OAuthRefreshToken{
